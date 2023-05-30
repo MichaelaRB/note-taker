bb162188bbbbbdff460c48a9bd80bfaee420d187
Michaela Brown <michaelarbrown4@gmail.com>
2023-05-30 13:40:36 -0500

    added code to the post request to create a new note and save it in the db.json file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const noteData = require('./db/db.json');
 const uniqid = require('uniqid');
 
@@ -8,28 +9,62 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-
-    console.info(`${req.method} request received to get the notes.html file.`);
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-
-    console.info(`${req.method} request received to get the index.html file.`);
 });
 
-
 app.get('/api/notes', (req, res) => {
-    res.json(noteData);
-    console.info(`${req.method} request received to get a note.`);
+    res.status(200).json(`${req.method} request received to get notes.`);
+    console.info(`${req.method} request received to get notes.`);
 });
 
 app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received to add a note.`);
     console.info(`${req.method} request received to add a note.`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uniqid(),
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+  
+          parsedNotes.push(newNote);
+  
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!'));
+        }
+      });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in saving notes');
+  }
 })
 
 app.listen(PORT, () => {
