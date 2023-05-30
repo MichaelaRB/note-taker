fbfe0bceaad78c75e26c9b52f706fe4ba9a425db
Michaela Brown <michaelarbrown4@gmail.com>
2023-05-30 14:59:37 -0500

    added code to for GET /api/notes to return the JSON for the notes, rearranged server.js file so the right GET is used.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const noteData = require('./db/db.json');
+const noteData = require('./db/db');
 const uniqid = require('uniqid');
 
 const PORT = 3001;
@@ -12,17 +12,28 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.get('/api/notes', (req, res) => {
-    res.status(200).json(`${req.method} request received to get notes.`);
-    console.info(`${req.method} request received to get notes.`);
+    console.info(`${req.method} request received to get reviews`);
+
+    return res.status(200).json(noteData);
+})
+
+app.get('/api/notes/:note_id', (req, res) => {
+    if (req.params.note_id) {
+        console.info(`${req.method} request received to get a single note`);
+        const noteId = req.params.note_id;
+        for (let i = 0; i < noteData.length; i++) {
+          const currentNote = noteData[i];
+          if (currentNote.note_id === noteId) {
+            res.status(200).json(currentNote);
+            return;
+          }
+        }
+        res.status(404).send('Review not found');
+      } else {
+        res.status(400).send('Review ID not provided');
+      }
 });
 
 app.post('/api/notes', (req, res) => {
@@ -66,6 +77,15 @@ app.post('/api/notes', (req, res) => {
     res.status(500).json('Error in saving notes');
   }
 })
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
